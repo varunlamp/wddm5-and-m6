a0065c89d19c81979576082c5bebe97aa679a350
varun <103111400+varunlamp@users.noreply.github.com>
2022-10-08 09:05:13 +0530

    Add files via upload

---
+++ addition.js
@@ -0,0 +1,7 @@
+function add() {
+var a = document.getElementById("firstno").value
+var b =  document.getElementById("secondno").value
+var result = a+b
+document.getElementById("result").value=result
+}
+
---
+++ substarct.js
@@ -0,0 +1,7 @@
+function subtract() {
+var a = document.getElementsById("firstno").value
+var b = document.getElementsById("secondno").value
+var result
+result = a-b
+document.getElementsById("result").value=result
+}
